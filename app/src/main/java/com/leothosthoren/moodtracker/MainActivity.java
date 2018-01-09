973b4a546a2fef049ad2cd897d5e11d93d505f9f
Leothos_Thoren <soussou.med@live.fr>
2018-01-09 18:39:56 +0100

    Try to add slide captor

--- app/src/main/java/com/leothosthoren/moodtracker/MainActivity.java
+++ app/src/main/java/com/leothosthoren/moodtracker/MainActivity.java
@@ -1,17 +1,28 @@
 package com.leothosthoren.moodtracker;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SWIPE_MIN_DISTANCE = 240;
+    private static final int SWIPE_MAX_OFF_PATH = 500;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 300;
     private ImageButton mBtnComment;
     private ImageButton mBtnHistory;
+    private ImageButton mBtnSmiley;
+    private RelativeLayout mRelativeLayout;
+    private GestureDetector gestureDetector;
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -20,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
 
         mBtnComment = (ImageButton) findViewById(R.id.main_activity_comment);
         mBtnHistory = (ImageButton) findViewById(R.id.main_activity_history);
+        mBtnSmiley = (ImageButton) findViewById(R.id.main_activity_img);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.main_activity_layout_backgound);
+
+        /*
+        *@mBtnCommentis a button on the left bottom wich open an AlertDialog on click
+        * */
 
         mBtnComment.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +45,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        gestureDetector = new GestureDetector(new MyGestureDetector());
+        View mainview = (View) mRelativeLayout;
+
+        // Set the touch listener for the main view to be our custom gesture listener
+        mainview.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
+
+        mBtnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent historyActivity = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(historyActivity);
+            }
+        });
     }
 
+    /*@addComment method
+    * Create an alert dialog with text input and two buttons 'Valider' and 'Annuler'
+    * */
     private void addComment() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -51,5 +88,34 @@ public class MainActivity extends AppCompatActivity {
         }).create()
                 .show();
     }
+
+    /*@ MyGestureDetector it checks when user swipe the screen*/
+    class MyGestureDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH) {
+                return false;
+            }
+            // Up to Down swipe
+            if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+
+                mRelativeLayout.setBackgroundColor(getColor(R.color.light_sage));
+                mBtnSmiley.setImageResource(R.mipmap.smiley_happy);
+
+                // Down to Up swipe
+            } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                mRelativeLayout.setBackgroundColor(getColor(R.color.banana_yellow));
+                mBtnSmiley.setImageResource(R.mipmap.smiley_super_happy);
+            }
+
+            return false;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+    }
+
 
 }
